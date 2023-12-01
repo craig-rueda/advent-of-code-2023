911a1dd034e19186924018dd274c96ad1a82cde7
Craig Rueda <craig@craigrueda.com>
2023-12-01 11:17:22 -0800

    Adding part 2

--- problem_1/sln.py
+++ problem_1/sln.py
@@ -1,14 +1,37 @@
 import re
 
-LEADING_NUM_PATTERN = re.compile("^[^\d]*(\d)")
-TRAILING_NUM_PATTERN = re.compile(".*(\d)[^\d]*$")
+NUM_MAP = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+# Build up a forward and reverse regex from our lookup table
+LEADING_NUM_PATTERN = re.compile(f"({'|'.join(NUM_MAP.keys())}|\d)")
+TRAILING_NUM_PATTERN = re.compile(
+    f"({'|'.join([key[::-1] for key in NUM_MAP.keys()])}|\d)"
+)
+
 
 sum = 0
 with open("input.txt", "r") as file:
     for line in file:
-        sum += int(
-            LEADING_NUM_PATTERN.match(line).group(1)
-            + TRAILING_NUM_PATTERN.match(line).group(1)
-        )
+        # Chomp off trailing newline
+        line = line.replace("\n", "")
+        leading = LEADING_NUM_PATTERN.search(line).group()
+        trailing = TRAILING_NUM_PATTERN.search(line[::-1]).group()
 
-print(f"Calibration value: {sum}")
+        # Convert val, if necessary
+        leading = leading if len(leading) == 1 else NUM_MAP[leading]
+        trailing = trailing if len(trailing) == 1 else NUM_MAP[trailing[::-1]]
+
+        print(f"{line} -> {leading}{trailing}")
+        sum += int(leading + trailing)
+
+print(f"\nCalibration value: {sum}")
